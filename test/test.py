9d3abd9fa76a700eec4252a229c88a432e18f76f
ngobiea <ngobiea@gmail.com>
2023-12-17 17:14:20 +0500

    tse-2 assignmet-3 webapp

--- test/test.py
+++ test/test.py
@@ -13,7 +13,7 @@ options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("http://3.6.160.154:80")
+driver.get("http://15.207.18.213")
 print(driver.title)
 try:
     # Test Case 1: Add a goal
